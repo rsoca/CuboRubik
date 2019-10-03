f0ae0babb95020dc571b7e2546d0d5d636668361
JuanManuelPalaciosNavas <juanma.palacios@hotmail.es>
2019-10-03 17:06:00 +0200

    Primera Version

---
+++ PracticaInteligentes/src/Cubo.java
@@ -0,0 +1,13 @@
+
+public class Cubo { //matriz de 3 dimensiones: la primera para decirme que cara es, la segunda y tercera son las dimensiones de la cara.
+	//IDENTIFICADORES DE CARAS QUE SERIAN LOS COLORES (tienen que ser short ha dicho el profe xd)
+	  private short UP=0, DOWN=1,LEFT=4,RIGHT=5,FRONT=2,BACK=3;
+	 //LOS MOVIMIENTOS SON DE Ln, Dn Y Bn
+	
+	
+}
+//metodos necesarios:
+//Clonar
+//Contructor que reciba el string del json
+//Generador de numero unico de MD5
+//Para comprobar si esta bien partir del cubo hecho, desacerlo y volver a hacer movimientos al reves para resolverlo a ver si sale bien.
