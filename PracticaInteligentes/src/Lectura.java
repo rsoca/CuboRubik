e125279b29a9448230de9bb1046b48809f0d3c06
rafa <rafa@172.19.240.240>
2019-10-09 13:07:15 +0200

    Cubo acabado

--- PracticaInteligentes/src/Lectura.java
+++ PracticaInteligentes/src/Lectura.java
@@ -13,20 +13,26 @@ public class Lectura {
 	private int cubo[][][];
 
 	public int[][][] leerArchivo() throws IOException {
-		// public static void main(String[] args) throws IOException{s
 
 		try {
 			String ob = FileUtils.readFileToString(new File("cuboresuelto.json"));
+			
 			JSONObject json = new JSONObject(ob);
+			
 			JSONArray arrayup = json.getJSONArray("UP");
 			JSONArray arraydown = json.getJSONArray("DOWN");
 			JSONArray arrayleft = json.getJSONArray("LEFT");
 			JSONArray arrayright = json.getJSONArray("RIGHT");
 			JSONArray arrayfront = json.getJSONArray("FRONT");
 			JSONArray arrayback = json.getJSONArray("BACK");
+			
 			int dimension = arrayup.length();
+			
 			cubo = new int[6][dimension][dimension];
-
+			
+			//System.out.println("DImensiones"+dimension);
+			// pilla bien las dimensiones
+			
 			for (int i = 0; i < arrayup.length(); i++) {
 				JSONArray f = arrayup.getJSONArray(i);
 				for (int j = 0; j < f.length(); j++) {
@@ -63,20 +69,39 @@ public class Lectura {
 					estado += f.get(j);
 				}
 			}
-			char[] numeros = estado.toCharArray();
+			
+			//hacemos un vector de string separando el de estado
+			String [] lista = estado.split("");
+			
+			//creamos el vector donde guardaremos los numeros con la longitud de la lista anterior
+			int [] numeros = new int [lista.length];
 			//int limite = 6 * dimension * dimension;
+			
+			//recorremos el string numeros y guardamos en cada posicion el estado correspondiente
+			for(int g=0;g<numeros.length;g++) {
+				numeros[g]=Integer.parseInt(lista[g]);
+			}
+			
+			
+			//imprimimos el array numeros
+			/*for(int i=0;i<numeros.length;i++) {
+				System.out.print(" "+numeros[i]);
+			}*/
+			
 			int contador = 0;
-			System.out.println(estado);
+			int limite = 6*dimension*dimension;
+			
 			for (int i = 0; i < 6; i++) {
 				for (int j = 0; j < dimension; j++) {
 					for (int k = 0; k < dimension; k++) {
-						//if (contador < limite) {
+						if (contador < limite) {
 							cubo[i][j][k] = numeros[contador];
-							System.out.println(cubo[i][j][k]);
-							//contador++;
-						//}
+							//System.out.println("Elcubo");
+							System.out.print(" "+cubo[i][j][k]);
+							contador++;
+						}
 					}
-				}contador++;
+				}
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
--- PracticaInteligentes/src/Main.java
+++ PracticaInteligentes/src/Main.java
@@ -7,15 +7,16 @@ public class Main {
 		Cubo c= new Cubo(leer.leerArchivo());
 		int [][][] tri = c.getPosiciones();
 		
-		//System.out.print("Longitud"+tri.+"Longitud en 0"+tri[0].length);
+		System.out.println("Longitud"+tri.length+"Longitud en 0 "+tri[0].length);
 		
-		/*for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < tri[0].length; j++) {
 				for (int k = 0; k < tri[0][0].length; k++) {
-					System.out.println(tri[i][j][k]);
+					System.out.print(" "+tri[i][j][k]);
 				}
+				System.out.println();
 			}
-		}*/
+		}
 	}
 
 }
