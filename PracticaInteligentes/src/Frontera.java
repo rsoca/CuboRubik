6c74799b85d7f54e70bca24f47864e3548ef5e0d
USUARIO <USUARIO@172.19.189.2>
2019-10-15 09:19:34 +0200

    Frontera

--- PracticaInteligentes/src/Frontera.java
+++ PracticaInteligentes/src/Frontera.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Frontera {
 	private static final int t_inicial = 500;
-	private List<Nodo> listaNodos;
+	private List<Nodo> listaNodos; //Lista de nodos
+	private List<Long> listaTiempo = new ArrayList<Long>(); //tiempos de insercion
+	
+	public void obtenerTiempos() {
+		
+	}
 	
 }
