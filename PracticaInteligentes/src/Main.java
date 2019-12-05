6d565c773b5a98db7d67ac4a01d920338c9ce934
USUARIO <USUARIO@DESKTOP-1NJ55O4>
2019-12-05 12:36:58 +0100

    PRÁCTICA FINALIZADA.

--- PracticaInteligentes/src/Main.java
+++ PracticaInteligentes/src/Main.java
@@ -2,40 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	static Scanner leer = new Scanner(System.in);
 
+	public static void main(String[] args) throws IOException {
 		Problema problema = new Problema();
-		Scanner leer = new Scanner(System.in);
-		int profundidad = 0;
 		int incProf = 1;
+		int profundidad = 0;
 		boolean salida = false;
-		char respuesta;
 		boolean poda = true;
 		do {
-			System.out.println("Introduzca el numero de una opcion: "
-					+ "\n 1. Estrategia A*"
-					+ "\n 2. Estrategia ANCHURA"
-					+ "\n 3. Estrategia PROFUNDIDAD ITERATIVA"
-					+ "\n 4. Estrategia PROFUNDIDAD ACOTADA"
-					+ "\n 5. Estrategia COSTE UNIFORME" 
-					+ "\n 6. Estrategia VORAZ"
-					+ "\n 7. SALIR");
-			int eleccion = leer.nextInt();
+			int eleccion = recogerEleccion();
 			if (eleccion >= 1 && eleccion <= 6) {
-				System.out.println("Introduzca la profundidad maxima:");
-				profundidad = leer.nextInt();
+				profundidad = recogerProfundidad();
 				if (eleccion == 3) {
 					System.out.println("Elija un incremento de profundidad");
 					incProf = leer.nextInt();
 				}
-				do {
-					System.out.println("¿Quieres poder podar los nodos del árbol? (S/N):");
-					respuesta = leer.next().toUpperCase().charAt(0);
-				} while (respuesta != 'S' && respuesta != 'N');
-
-				if (respuesta == 'N') {
-					poda = false;
-				}
+				poda = recogidaPoda();
 			}
 
 			switch (eleccion) {
@@ -68,6 +51,63 @@ public class Main {
 		} while (salida == false);
 
 		leer.close();
+	}
+
+	private static boolean recogidaPoda() {
+		char respuesta = 0;
+		boolean poda = true;
+		boolean correcto = false;
+		while (!correcto) {
+			System.out.println("¿Quieres poder podar los nodos del árbol? (S/N):\n");
+			respuesta = leer.next().toUpperCase().charAt(0);
+			if (respuesta != 'S' && respuesta != 'N') {
+				System.out.println("Tiene que ser Si o No\n");
+				correcto = false;
+			} else
+				correcto = true;
+
+		}
+		if (respuesta == 'N') {
+			poda = false;
+		}
+		return poda;
+	}
+
+	private static int recogerProfundidad() {
+		boolean correcto = false;
+		int profundidad = 0;
+		System.out.println("Introduzca la profundidad maxima:");
+		while (!correcto) {
+			try {
+				profundidad = leer.nextInt();
+				correcto = true;
+			} catch (InputMismatchException e) {
+				System.out.println("El valor introducido tiene que ser un número. Prueba otra vez.\n");
+				leer.nextLine();
+			}
+
+		}
+		return profundidad;
+	}
+
+	private static int recogerEleccion() {
+		boolean correcto = false;
+		int eleccion = 0;
+		System.out.println("Introduzca el numero de una opcion: " + "\n 1. Estrategia A*" + "\n 2. Estrategia ANCHURA"
+				+ "\n 3. Estrategia PROFUNDIDAD ITERATIVA" + "\n 4. Estrategia PROFUNDIDAD ACOTADA"
+				+ "\n 5. Estrategia COSTE UNIFORME" + "\n 6. Estrategia VORAZ" + "\n 7. SALIR");
+		while (!correcto) {
+			try {
+				eleccion = leer.nextInt();
+				correcto = true;
+			} catch (InputMismatchException e) {
+				System.out.println("El valor introducido tiene que ser un número. Prueba otra vez.\n");
+				leer.nextLine();
+			}
+
+		}
+
+		return eleccion;
 	}
 
 }
